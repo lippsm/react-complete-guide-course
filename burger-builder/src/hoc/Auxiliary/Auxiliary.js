564a05726e2dfedcb65e82f4c53948e36aae011d
lippsm <michaeljlipps@gmail.com>
2018-04-19 12:04:14 -0400

    Reorganized folder structure

---
+++ burger-builder/src/hoc/Auxiliary/Auxiliary.js
@@ -0,0 +1,6 @@
+// This file could probably be eliminated by replacing <Aux>
+// tags with <React.Fragment> tags (as of react 16.2)
+// Using this for brevity and to match the course
+const auxiliary = (props) => props.children;
+
+export default auxiliary;
