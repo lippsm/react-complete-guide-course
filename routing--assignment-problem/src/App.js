2760344e806f8fab05708c0b34ac280fd9bd7cbd
lippsm <michaeljlipps@gmail.com>
2018-04-20 16:36:42 -0400

    got courses and users links working

--- routing--assignment-problem/src/App.js
+++ routing--assignment-problem/src/App.js
@@ -3,10 +3,11 @@ import { Route, NavLink } from "react-router-dom";
 
 import Courses from "./containers/Courses/Courses";
 import Users from "./containers/Users/Users";
+import './App.css';
 
 /*
-  <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-  <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+  DONE<li >Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
+  DONE<li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
   <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
   <li>Pass the course ID to the "Course" page and output it there</li>
   <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
