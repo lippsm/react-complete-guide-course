7158815fd2261644f75d378db5c57bfab03ef83a
lippsm <michaeljlipps@gmail.com>
2018-04-24 13:24:06 -0400

    Redux learning projects

---
+++ redux--01-start/src/store/reducers/result.js
@@ -0,0 +1,26 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+  results: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    //using concat because it creates a new array, push mutates the existing array
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result })
+      };
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(result => result.id !== action.resultId);
+      return {
+        ...state,
+        results: updatedArray
+      };
+    default:
+  }
+  return state;
+};
+
+export default reducer;
---
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -0,0 +1,33 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  persons: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: action.payload.name,
+        age: action.payload.age
+      };
+
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      };
+
+    case actionTypes.DELETE_PERSON:
+      const updatedArray = state.persons.filter(person => person.id !== action.personId);
+      return {
+        ...state,
+        persons: updatedArray
+
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
